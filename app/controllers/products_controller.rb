933323bb3565bf81ab915b8649ae9c1908652d5d
borneobuzz <borneobuzz@outlook.com>
2017-04-21 21:14:25 +0000

    4.2 Add price to product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -62,7 +62,7 @@ class ProductsController < ApplicationController
   end
   
   def product_params
-    params.require(:product).permit(:name, :description, :image_url, :color)
+    params.require(:product).permit(:name, :description, :image_url, :colour, :price) #adding a new data field column on this line doesn't seem make it visible on the page.
   end
 
   private
@@ -73,6 +73,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url, :colour)
+      params.require(:product).permit(:name, :description, :image_url, :colour, :price) #but adding field on this line makes data visible on the product page.
     end
 end
