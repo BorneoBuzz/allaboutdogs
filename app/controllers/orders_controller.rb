67c4a21dd052180c2f9349ae6031fac0e3f7b369
borneobuzz <borneobuzz@outlook.com>
2017-04-28 03:49:46 +0000

    4.3 created order controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController #this inherits all methods in application_controller.rb file
+    
+    def create
+    end
+    
+    def index
+    end
+    
+    def show
+    end
+    
+    def new
+    end
+    
+    def destroy
+    end
+    
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,14 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   get 'static_pages/index'
+  
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root 'static_pages#landing_page'
+  root 'static_pages#landing_page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
