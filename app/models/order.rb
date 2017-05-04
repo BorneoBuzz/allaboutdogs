8236068d88eda12a067e7e82c964df668421be8d
borneobuzz <borneobuzz@outlook.com>
2017-05-04 20:42:40 +0000

    4.4 orders and product database model and association

---
+++ app/models/order.rb
@@ -0,0 +1,5 @@
+class Order < ActiveRecord::Base 
+    belongs_to :product
+end
+#4.3 example shows ApplicationRecord, however, product.rb showed ActiveRecord::Base instead.
+#Modifying 4.3 example from ApplicationRecord to ActiveRecord::Base
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+    has_many :orders
 end
+
+
+
+
+
