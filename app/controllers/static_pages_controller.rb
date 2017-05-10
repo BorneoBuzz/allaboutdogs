dea88584af06d39a065a912820b43937bb065be4
borneobuzz <borneobuzz@outlook.com>
2017-05-10 20:59:00 +0000

    4.5 Display dynamic data in image carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,14 @@ class StaticPagesController < ApplicationController
   def index
   end
   
+  #def landing_page
+   # @featured_product = Product.first
+  #end - Original coding-commented out on 5/10/17. See below per exercise
+  
   def landing_page
-    @featured_product = Product.first
-  end
+   @products = Product.limit(3)
+  end - Original coding-commented out on 5/10/17. See below per exercise
+  
+  
   
 end
