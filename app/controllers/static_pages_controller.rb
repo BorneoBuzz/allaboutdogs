2524a732027cff92c9c3c1311732132c442792ed
borneobuzz <borneobuzz@outlook.com>
2017-09-26 14:02:29 +0000

    5.2 Making search case insensitive in product model

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,5 +8,18 @@ class StaticPagesController < ApplicationController
   
   def landing_page
    @products = Product.limit(3)
-  end 
+  end
+  
+  #Purely for exercise 5.3
+  ##def thank_you
+   ##@name = params[:name]
+   ##@email = params[:email]
+   ##@message = params[:message]
+   ##ActionMailer::Base.mail(from: @email,
+     ## to: 'your-email@example.com',
+      ##subject: "A new contact form message from #{@name}",
+      ##body: @message).deliver_now
+  ## end
+  
 end
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,11 @@ class Product < ActiveRecord::Base
     has_many :orders
     
     def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
+        if Rails.env.production?
+            Product.where("name ILIKE ?", "%#{search_term}%")
+        elsif Rails.env.development?
+            Product.where("name LIKE ?", "%#{search_term}%")
+        end
     end
 end
 
