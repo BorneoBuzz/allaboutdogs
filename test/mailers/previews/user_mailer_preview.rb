b491f520fbff8dd9adc7f4e33b30b9a6c44cd6f3
borneobuzz <borneobuzz@outlook.com>
2017-10-03 21:04:51 +0000

    5.3

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,7 +1,16 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-    def contact_form
-        UserMailer.contact_form("joymajanil@gmail.com", "Joyce", "HELLO WORLD!")
+    #def contact_form
+    #    UserMailer.contact_form("joymajanil@gmail.com", "Joyce", "HELLO WORLD!")
+    #end
+    
+    default from: "joymajanil@gmail.com"
+    
+    def contact_form(email, name, message)
+        @message = message
+        mail(from: email,
+            to: 'joymajanil@gmail.com',
+            subject: "A new contact form message from #{name}")
     end
 
 end
