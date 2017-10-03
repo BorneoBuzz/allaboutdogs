fb286a0d8d8be7996e6160dd862a78e4c341f6ae
borneobuzz <borneobuzz@outlook.com>
2017-10-03 20:50:09 +0000

    5.3

--- config/environment.rb
+++ config/environment.rb
@@ -6,13 +6,6 @@ Rails.application.initialize!
 
 
 ActionMailer::Base.smtp_settings ={
-    #:port           => ENV["MAILGUN_SMTP_PORT"], 
-    #:address        => ENV["MAILGUN_SMTP_SERVER"],
-    #:user_name      => ENV["MAILGUN_SMTP_LOGIN"], 
-    #:password       => ENV["MAILGUN_SMTP_PASSWORD"],
-    #:domain         => 'allaboutdogs.herokuapp.com',
-    #:authentication => :plain,
-    
     address: 'smtp.sendgrid.net',
     port: '587',
     authentication: :plain,
@@ -23,3 +16,11 @@ ActionMailer::Base.smtp_settings ={
 }
 
 ActionMailer::Base.delivery_method = :smtp
+
+   #:port           => ENV["MAILGUN_SMTP_PORT"], 
+    #:address        => ENV["MAILGUN_SMTP_SERVER"],
+    #:user_name      => ENV["MAILGUN_SMTP_LOGIN"], 
+    #:password       => ENV["MAILGUN_SMTP_PASSWORD"],
+    #:domain         => 'allaboutdogs.herokuapp.com',
+    #:authentication => :plain,
+    
