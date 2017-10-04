ca50bcd924cf96d37cd0e8296551de39d7222b6a
borneobuzz <borneobuzz@outlook.com>
2017-10-04 01:36:17 +0000

    5.3 html5 form validation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,6 @@ class StaticPagesController < ApplicationController
   def index
   end
   
-  #def landing_page
-   # @featured_product = Product.first
-  #end - Original coding-commented out on 5/10/17. See below per exercise
   
   def landing_page
    @products = Product.limit(3)
